4b4ce7dd894b1cfcd421cdaa842b583ad4cfdd01
Michael Quinn <Michael.L.Quinn20@gmail.com>
2020-09-22 18:53:54 -1000

    Added models and tests

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Loan(models.Model):
+    title = models.CharField(max_length=75)
+    description = models.CharField(max_length=250)
+    principle = models.FloatField()
+    current_balance = models.FloatField()
+    apr = models.FloatField()
+    is_paid = models.BooleanField(default=False)
+    start_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+class Payment(models.Model):
+    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE)
+    payment_amount = models.FloatField()
+    payment_date = models.DateField()
--- calculator/tests.py
+++ calculator/tests.py
@@ -1,8 +1,51 @@
 from django.test import TestCase
+from calculator.models import Loan,Payment
+from django.utils.timezone import now
 
-class CalculatorTests(TestCase):
+class CalculatorViewTests(TestCase):
 
     def test_loanpageview(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         print("Loanview status code is 200.")
+
+class LoanModelTests(TestCase):
+
+    def setUp(self):
+        Loan.objects.create(
+            title="test title",
+            description='test desc',
+            principle=1000,
+            current_balance=900,
+            apr=10,
+            is_paid=False,
+            start_date=now(),
+        )
+
+    def test_loancreation(self):
+        loan = Loan.objects.first()
+        self.assertIsNotNone(loan)
+        print("Loan created.")
+
+class PaymentModelTests(TestCase):
+
+    def setUp(self):
+        Loan.objects.create(
+            title="test title",
+            description='test desc',
+            principle=1000,
+            current_balance=900,
+            apr=10,
+            is_paid=False,
+            start_date=now(),
+        )
+        Payment.objects.create(
+            loan_id=Loan.objects.first(),
+            payment_amount=100,
+            payment_date=now(),
+        )
+
+    def test_paymentcreation(self):
+        payment=Payment.objects.first()
+        self.assertIsNotNone(payment)
+        print("Payment created.")
