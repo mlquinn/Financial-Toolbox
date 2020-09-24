a74fe0df2618a17fcf22bebfbe029015a9c571dc
Michael Quinn <Michael.L.Quinn20@gmail.com>
2020-09-24 13:23:44 -1000

    Added 'create new loan' functionality.

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LoanDashView, LoanDetailView
+from .views import LoanDashView, LoanDetailView, NewLoanView
 app_name='calculator'
 urlpatterns = [
     path('dash/', LoanDashView.as_view(), name='loan-dash'),
+    path('new/', NewLoanView.as_view(), name='loan-new'),
     path('<int:pk>/', LoanDetailView.as_view(), name='loan-detail'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
 from .models import Loan, Payment
 
@@ -16,3 +16,12 @@ class LoanDashView(ListView):
 class LoanDetailView(DetailView):
     model = Loan
     template_name='calculator/loanview.html'
+
+class NewLoanView(CreateView):
+    model = Loan
+    template_name='calculator/newloan_form.html'
+    fields = '__all__'
+    
+
+    def get_success_url(self):
+        return reverse('calculator:loan-detail', kwargs={'pk':self.object.pk})
